c5b34d3ac5b0780b14bf83d587bda44300c06ff6
Barış Sokat <barissokat@outlook.com>
2020-08-13 23:23:35 +0300

    Record model changes

--- app/Project.php
+++ app/Project.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Project extends Model
 {
@@ -12,6 +13,13 @@ class Project extends Model
      * @var array
      */
     protected $guarded = [];
+
+    /**
+     * The project's old attributes.
+     *
+     * @var array
+     */
+    public $old = [];
 
     /**
      * The owner of the project.
@@ -61,7 +69,26 @@ class Project extends Model
      */
     public function recordActivity($description)
     {
-        $this->activity()->create(compact('description'));
+        $this->activity()->create([
+            'description' => $description,
+            'changes' => $this->activityChanges($description),
+        ]);
+    }
+
+    /**
+     * Fetch the changes to the model.
+     *
+     * @param  string $description
+     * @return array|null
+     */
+    protected function activityChanges($description)
+    {
+        if ($description == 'updated') {
+            return [
+                'before' => Arr::except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
+                'after' => Arr::except($this->getChanges(), 'updated_at'),
+            ];
+        }
     }
 
     /**
